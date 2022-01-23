174411bdb53b4c10e625612155f5a581016eeb42
Kevin Woodman <kwoodman22@priorypanther.com>
2022-01-22 17:19:03 -0800

    Seperating hood into another subsystem

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -9,28 +9,29 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
 public class Shoot extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+
   private final Shooter subsytem;
 
-  /**
-   * Creates a new Shooter Command. 
-   * Author: Kevin Woodman
-   * 
-   * @param subsystem The current instance shooter subsystem 
-   */
   public Shoot(Shooter subsystem) {
     this.subsytem = subsystem;
-    // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    
+
+  }
+
+  //-----------------Unused for now-----------------
 
   // Called once the command ends or is interrupted.
   @Override
